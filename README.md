963e46924103c793d9ea8bf5f6652db96d97545c
gabiarrieta <gabriela.arrieta.187@gmail.com>
2021-06-09 17:56:31 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+### 🧆🥘 FOOD RECIPE APP🥘🧆 
+
+- Created with React.js, using [Recipe Search APi][1] and Axios
+
+**Preview**
+![Food App](Food-App.png "Food App")
+
+### Installation
+
+-Clone the repository
+
+-To install and run this proyect just type and execute
+
+`npm install`
+
+-Start the proyect with
+
+`npm start`
+
+👩‍💻
+
+
+
+[1]: https://developer.edamam.com/edamam-docs-recipe-api "Recipe Search API "
---
+++ src/App.js
@@ -0,0 +1,52 @@
+import "./App.css";
+import { useState } from "react";
+import RecipeTile from "./components/recipe-tile";
+
+//import axios
+import Axios from "axios";
+
+function App() {
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  const YOUR_APP_ID = "e0178b1e";
+  const YOUR_APP_KEY = "69595dbeaa699d9df2757eecdea0df22";
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
+
+  const getRecipes = async () => {
+    const result = await Axios.get(url);
+    // console.log(result.data)
+    setRecipes(result.data.hits);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getRecipes();
+  };
+
+  return (
+    <div className="app">
+      <h1>🥣Food Recipes🥣</h1>
+      <form className="app__searchForm" onSubmit={onSubmit}>
+        <input
+          className="app__input"
+          type="text"
+          placeholder="enter ingridient"
+          autoComplete="Off"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <input className="app__submit" type="submit" value="Search" />
+      </form>
+
+      <div className="app__recipes">
+        {recipes !== [] &&
+          recipes.map((recipe) => {
+            return <RecipeTile recipe={recipe} />;
+          })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
